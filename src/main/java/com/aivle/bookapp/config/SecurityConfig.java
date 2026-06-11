4e855109fbcfa3baeccd7a70cf258f6822adf608
Jiham Park <jihamp@vt.edu>
2026-06-11 11:41:06 +0900

    author user로 변경

--- src/main/java/com/aivle/bookapp/config/SecurityConfig.java
+++ src/main/java/com/aivle/bookapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,10 +39,10 @@ public class SecurityConfig {
                         }
                 ))
 
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/users/register", "/users/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/books/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/hello", "/hello/**", "/greet").permitAll()
                         .anyRequest().authenticated()
                 )
 
